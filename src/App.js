ec8307100656095b0585cff9eb88faf308463d60
bee60990 <155386705+bee60990@users.noreply.github.com>
2025-02-28 15:32:07 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="Weather_app"></div>;
+  return (
+    <div className="Weather_app">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Julia and can be found on{" "}
+        <a href="" target="_blank">
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
