4ead1dded672f878117b6f0458be390607bb1499
bee60990 <155386705+bee60990@users.noreply.github.com>
2025-03-01 20:53:05 +0100

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="Weather_app">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Julia and can be found on{" "}
-        <a href="" target="_blank">
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+
+        <footer>
+          This project was coded by Julia and can be found on{" "}
+          <a href="" target="_blank">
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
